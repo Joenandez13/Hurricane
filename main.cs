c221f6157ebe41580a27699a8edfa4f10dc60476
Joenandez13 <143644911+Joenandez13@users.noreply.github.com>
2023-09-24 23:37:42 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Enter wind speed: ");
+    double speed = Convert.ToDouble(Console.ReadLine());
+
+    if (speed >= 157){
+      Console.WriteLine("Category Five Hurricane");
+      }
+    else if (speed >= 130) {
+      Console.WriteLine("Category Four Hurricane");
+      }
+    else if (speed >= 111) {
+      Console.WriteLine("Category Three Hurricane");
+      }
+    else if (speed >= 96) {
+      Console.WriteLine("Category Two Hurricane");
+      }
+    else if (speed >= 74) {
+      Console.WriteLine("Category One Hurricane");
+      }
+    else {
+      Console.WriteLine("Not a Hurricane");
+    }
+  }
+}
